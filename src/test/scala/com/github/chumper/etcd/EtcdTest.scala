30ed163e1a5df0e653cb6a47314f641af7943b1a
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-21 14:47:30 +0100

    added service discovery for client

--- src/test/scala/com/github/chumper/etcd/EtcdTest.scala
+++ src/test/scala/com/github/chumper/etcd/EtcdTest.scala
@@ -136,4 +136,17 @@ class EtcdTest extends AsyncFunSuite with BeforeAndAfter with ParallelTestExecut
     }
     p.future
   }
+
+  test("Etcd can watch a key with prefix") {
+    val p = Promise[Assertion]
+    var watchId = 0l
+    etcd.watch.prefix("12345") { resp =>
+      watchId = resp.watchId
+      p.success(assert(true))
+    }
+    etcd.kv.putString("12345.asdasd", "12345") map { resp =>
+      etcd.watch.cancel(watchId)
+    }
+    p.future
+  }
 }
